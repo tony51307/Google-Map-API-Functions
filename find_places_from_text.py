b20224ca2ac98ac4682d5376a501c1c7e70152a3
tony51307 <59693085+tony51307@users.noreply.github.com>
2020-10-03 15:47:41 +0800

    Add files via upload

---
+++ find_places_from_text.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 15:45:53 2020
+
+@author: tony51307
+"""
+
+def find_places_from_text(place_name, api_key, language):
+    inputtyoe = 'textquery'
+    url = ('https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
+            '?key=%s'
+            '&inputtype=%s'
+            '&input=%s'
+          '&language=%s') % (api_key, inputtype, place_name, language)
+    response = requests.get(url)
+    jsonData = response.json()
+    id_list = []
+    for i in jsonData['candidates']:
+        id_list.append(i['place_id'])
+    # return a list of place IDs
+    return id_list
+
